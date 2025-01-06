416b37c5755e5d5a90b100c3a9dce8f2395d5895
einav <einav.manor@post.runi.ac.il>
2025-01-07 00:36:13 +0200

    upload Homework9-NotFinished

--- User.java
+++ User.java
@@ -43,23 +43,28 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
-        for(int i =0 ; i < getfFollows().length;){
+        for(int i =0 ; i < follows.length;){
             if(name.equals(getfFollows()[i])){
                 return true;
             }else{
                 i++;
             }
         }
-        return false;
+        if(follows.length == 0){
+            return false;
+        }else{
+            return false;
+        }
     }
+
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (follows(name) || fCount >= maxfCount) {
+        if (follows(name) == true || fCount >= maxfCount) {
             return false;
         }
-        follows[fCount] = name;
         fCount++;
+        follows[fCount -1] = name;
         return true;
         } 
 
